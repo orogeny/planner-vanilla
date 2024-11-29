a82470971773c95c2410f5f0b918b5b41490dd5e
Paul <paulwharin@gmail.com>
2024-11-29 07:33:32 +0000

    refactor: update palette to generate the html elements

--- src/components/palette.ts
+++ src/components/palette.ts
@@ -1,23 +1,44 @@
-function setup() {
-  const items = document.querySelectorAll<HTMLDivElement>(
-    "div.item.track[draggable='true']",
-  );
+import { StraightSpec } from "../track/straight";
+
+function create(title: string, items: StraightSpec[]) {
+  const paletteDiv = document.createElement("div");
+  paletteDiv.classList.add("palette");
+
+  const titleP = document.createElement("p");
+  paletteDiv.appendChild(titleP);
+  titleP.innerText = title;
+  titleP.classList.add("title");
+
+  const itemsUList = document.createElement("ul");
+  paletteDiv.appendChild(itemsUList);
+  itemsUList.classList.add("items");
 
   for (const item of items) {
-    item.ondragstart = handleDragStart;
+    const itemLI = document.createElement("li");
+    itemsUList.appendChild(itemLI);
+
+    const itemDiv = document.createElement("div");
+    itemLI.appendChild(itemDiv);
+    itemDiv.classList.add("item", "track", "straight");
+    itemDiv.draggable = true;
+    itemDiv.dataset.trackId = item.id;
+
+    itemDiv.ondragstart = (ev: DragEvent) => {
+      ev.dataTransfer?.setData("text/plain", `track#${item.id}`);
+    };
+
+    const labelSpan = document.createElement("span");
+    itemDiv.appendChild(labelSpan);
+    labelSpan.classList.add("label");
+    labelSpan.innerText = item.label;
+
+    const catnoSpan = document.createElement("span");
+    itemDiv.appendChild(catnoSpan);
+    catnoSpan.classList.add("catno");
+    catnoSpan.innerText = item.catno;
   }
+
+  return paletteDiv;
 }
 
-// Drag Event handlers
-
-function handleDragStart(ev: DragEvent) {
-  const item = ev.currentTarget as HTMLDivElement;
-
-  const trackId = item.dataset.trackId;
-
-  if (trackId) {
-    ev.dataTransfer!.setData("text/plain", `track#${trackId}`);
-  }
-}
-
-export { setup };
+export { create };
